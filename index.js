32a271b546e0e8066d6214129ed6cc11dafcc650
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-11-25 20:42:48 +0600

    token generation for user implemente

--- index.js
+++ index.js
@@ -29,16 +29,26 @@ const run = async () => {
     try {
         app.post('/users', async (req, res) => {
             const user = req.body
-            console.log(user)
+            // console.log(user)
             const storedUser = await usersCollection.findOne(user)
             if (storedUser) {
-                console.log(storedUser);
+                // console.log(storedUser);
                 return res.send({ acknowledged: true })
             }
             const result = await usersCollection.insertOne(user)
             // console.log(result);
             res.send(result)
+        })
 
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: "10h" });
+                return res.send({ accessToken: token })
+            }
+            return res.status(403).send({ accessToken: "" })
         })
 
     }
