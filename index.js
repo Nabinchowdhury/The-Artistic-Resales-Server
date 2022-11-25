8f53368605f8815a08173d2afb711b61ad841fd0
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-11-26 02:38:44 +0600

    delete user added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 
 const app = express()
@@ -88,10 +88,24 @@ const run = async () => {
             }
             return res.status(403).send({ accessToken: "" })
         })
+
+        app.get("/users/checkRole/:email", verifyJwt, async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            res.send({ role: user?.role })
+        })
+
         app.get('/users', verifyJwt, verifyAdmin, async (req, res) => {
             const role = req.query.role
             const query = { role: role }
             const result = await usersCollection.find(query).toArray()
+            res.send(result)
+        })
+        app.delete('/users/:id', verifyJwt, verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await usersCollection.deleteOne(query)
             res.send(result)
         })
 
